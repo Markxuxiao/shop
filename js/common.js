5d9712f728ff566dba5d6b06e77a27ff42221910
许啸 <515989252@qq.com>
2016-11-13 15:25:40 +0800

    购物车

--- js/common.js
+++ js/common.js
@@ -33,7 +33,48 @@
 // }
 // 全局函数
 var G = {
-    is_login:function(){
+    number_format:function(num, ext){
+        if(ext < 0){
+            return num;
+        }
+        num = Number(num);
+        if(isNaN(num)){
+            num = 0;
+        }
+        var _str = num.toString();
+        var _arr = _str.split('.');
+        var _int = _arr[0];
+        var _flt = _arr[1];
+        if(_str.indexOf('.') == -1){
+            /* 找不到小数点，则添加 */
+            if(ext == 0){
+                return _str;
+            }
+            var _tmp = '';
+            for(var i = 0; i < ext; i++){
+                _tmp += '0';
+            }
+            _str = _str + '.' + _tmp;
+        }else{
+            if(_flt.length == ext){
+                return _str;
+            }
+            /* 找得到小数点，则截取 */
+            if(_flt.length > ext){
+                _str = _str.substr(0, _str.length - (_flt.length - ext));
+                if(ext == 0){
+                    _str = _int;
+                }
+            }else{
+                for(var i = 0; i < ext - _flt.length; i++){
+                    _str += '0';
+                }
+            }
+        }
+
+        return _str;
+    }
+    ,is_login:function(){
         if(true){
            return true; 
        }else{
